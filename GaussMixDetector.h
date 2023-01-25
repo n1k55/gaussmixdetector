3eb5e0cd2d1d7bc512135523b984e8a20030a8a9
Nikita Kiryushin <nikss93@gmail.com>
2023-01-26 01:40:45 +0300

    Fix cppcoreguidelines-special-member-functions, hicpp-special-member-functions

--- GaussMixDetector.h
+++ GaussMixDetector.h
@@ -37,7 +37,6 @@ class GaussMixDetector
 public:
 	GaussMixDetector() = default;
 	explicit GaussMixDetector ( unsigned int _historyLength, double _initDeviation = defaultDeviation, double _T = defaultT, double _Cf = defaultCf );
-	~GaussMixDetector() = default;
 
 	void getMotionPicture( const cv::Mat& frame, cv::Mat& motion, bool cleanup = true );
 
